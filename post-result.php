282b544b221cd9f162dfcf19a585501f71822df3
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-09-15 19:34:40 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,16 +4,16 @@ include "view-header.php";
 ?>
     <h1>Post Result</h1>
 <?php
-if (isset($_POST['input-name'])) {
-  ?>
-  <p>The value sent is: </p>
-  <?php
-  echo $_POST['input-name'];
-}
-else {
-  ?>
-  <p>Nothing posted to the page.</p>
-  <?php
-}
+echo getDisplay();
 include "view-footer.php";
+
+function getDisplay() 
+{
+    if (isset($_POST['input-name'])) {
+      return "<p>The value sent is: </p>" . $_POST['input-name'];
+    }
+    else {
+    return "<p>Nothing posted to the page.</p>";
+    }
+}
 ?>
